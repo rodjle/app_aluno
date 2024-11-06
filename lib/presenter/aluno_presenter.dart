ee523d5b1ce72da1eac8c551d446febca4382067
unknown <rodjle@gmail.com>
2024-11-06 14:23:11 -0300

    config firebase

--- lib/presenter/aluno_presenter.dart
+++ lib/presenter/aluno_presenter.dart
@@ -1,5 +1,6 @@
 import 'dart:convert'; // Importa o pacote para codificação e decodificação JSON.
 import 'package:app_aluno/model/aluno.dart'; // Importa o modelo Aluno.
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:http/http.dart'
     as http; // Importa o pacote HTTP para fazer requisições web.
 import '../view/aluno_view.dart'; // Importa a interface de visualização AlunoView.
@@ -7,6 +8,8 @@ import '../view/aluno_view.dart'; // Importa a interface de visualização Aluno
 class AlunoPresenter {
   final AlunoView
       view; // Instância de AlunoView para comunicação entre o Presenter e a View.
+  final CollectionReference alunosRef =
+      FirebaseFirestore.instance.collection('alunos');
 
   // Construtor da classe AlunoPresenter, que inicializa a instância de view.
   AlunoPresenter(this.view);
@@ -65,6 +68,40 @@ class AlunoPresenter {
     } catch (e) {
       // Captura exceções e exibe a mensagem de erro.
       view.showError('Erro: $e');
+    }
+  }
+
+  //firebase
+  // Método que busca os alunos do Firestore.
+  Future<void> fetchAlunosFirebase() async {
+    try {
+      // Obtém os dados da coleção 'alunos' no Firestore.
+      QuerySnapshot snapshot = await alunosRef.get();
+
+      // Mapeia os dados para uma lista de objetos Aluno.
+      List<Aluno> alunos = snapshot.docs.map((doc) {
+        return Aluno.fromJson(doc.data() as Map<String, dynamic>);
+      }).toList();
+
+      // Exibe a lista de alunos na view.
+      view.displayAlunos(alunos);
+    } catch (e) {
+      // Captura exceções e exibe a mensagem de erro.
+      view.showError('Erro ao buscar dados: $e');
+    }
+  }
+
+  // Método para adicionar um aluno ao Firestore.
+  Future<void> addAlunoFirebase(Aluno aluno) async {
+    try {
+      // Adiciona um novo documento na coleção 'alunos'.
+      await alunosRef.add(aluno.toJson());
+
+      // Atualiza a lista após adicionar o aluno.
+      fetchAlunos();
+    } catch (e) {
+      // Captura exceções e exibe a mensagem de erro.
+      view.showError('Erro ao adicionar aluno: $e');
     }
   }
 }
